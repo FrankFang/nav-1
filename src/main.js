0d9bd6d5d940bb77f54ae1e1d1df3b764e05e050
frankfang <frankfang1990@gmail.com>
2019-11-03 18:11:52 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,47 +1,68 @@
-const $siteList = $(".siteList");
-const $lastLi = $siteList.find("li.last");
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
 const hashMap = xObject || [
-  { logo: "A", logoType: "text", url: "https://www.acfun.cn" },
-  {
-    logo: "./images/bilibili.png",
-    logoType: "image",
-    url: "https://www.bilibili.com"
-  }
-];
+  {logo: 'A', url: 'https://www.acfun.cn'},
+  {logo: 'B', url: 'https://www.bilibili.com'}
+]
+const simplifyUrl = (url) => {
+  return url.replace('https://', '')
+    .replace('http://', '')
+    .replace('www.', '')
+    .replace(/\/.*/, '') // 删除 / 开头的内容
+}
 
-const render = ()=>{
+const render = () => {
   $siteList.find('li:not(.last)').remove()
-  hashMap.forEach(node => {
+  hashMap.forEach((node, index) => {
     const $li = $(`<li>
-          <a href="${node.url}">
-            <div class="site">
-              <div class="logo">${node.logo[0]}</div>
-              <div class="link">${node.url}</div>
-            </div>
-          </a>
-    </li>`).insertBefore($lastLi);
-  });
+      <div class="site">
+        <div class="logo">${node.logo}</div>
+        <div class="link">${simplifyUrl(node.url)}</div>
+        <div class="close">
+          <svg class="icon">
+            <use xlink:href="#icon-close"></use>
+          </svg>
+        </div>
+      </div>
+    </li>`).insertBefore($lastLi)
+    $li.on('click', () => {
+      window.open(node.url)
+    })
+    $li.on('click', '.close', (e) => {
+      e.stopPropagation() // 阻止冒泡
+      hashMap.splice(index, 1)
+      render()
+    })
+  })
 }
 
 render()
 
-$(".addButton").on("click", () => {
-  let url = window.prompt("请问你要添加的网址是啥？");
-  if (url.indexOf("http") !== 0) {
-    url = "https://" + url;
+$('.addButton').on('click', () => {
+  let url = window.prompt('请问你要添加的网址是啥？')
+  if (url.indexOf('http') !== 0) {
+    url = 'https://' + url
   }
-  console.log(url);
+  console.log(url)
   hashMap.push({
-    logo: url[0],
-    logoType: "text",
+    logo: simplifyUrl(url)[0].toUpperCase(),
     url: url
-  });
+  })
   render()
-});
+})
 
-window.onbeforeunload = ()=>{
+window.onbeforeunload = () => {
   const string = JSON.stringify(hashMap)
   localStorage.setItem('x', string)
 }
+
+$(document).on('keypress', (e) => {
+  const {key} = e
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLowerCase() === key) {
+      window.open(hashMap[i].url)
+    }
+  }
+})
